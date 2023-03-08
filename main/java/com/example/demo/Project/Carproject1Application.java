2aa83204adeb8fe1a87eabe71f51933029b2d5b1
keerthanasriniv <117429428+keerthanasriniv@users.noreply.github.com>
2023-03-08 16:02:48 +0530

    Add files via upload

---
+++ main/java/com/example/demo/Project/Carproject1Application.java
@@ -0,0 +1,13 @@
+package com.example.demo.Project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Carproject1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Carproject1Application.class, args);
+	}
+
+}
---
+++ test/java/com/example/demo/Project/Carproject1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demo.Project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Carproject1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
